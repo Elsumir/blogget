2dcbf1d83887eedb43cdf2e386b96941e3ec9c62
Serik <askaroff_serik@mail.ru>
2023-05-06 10:04:39 +0300

    fix: работа с API

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,13 +1,71 @@
 import style from './Auth.module.css';
 import PropTypes from 'prop-types';
 import {ReactComponent as LoginIcon} from './img/login.svg';
+import {urlAuth} from '../../../api/auth';
+import {Text} from '../../../ui/Text';
+import {useEffect, useState} from 'react';
+import {URL_API} from '../../../api/const';
 
-export const Auth = ({auth}) => (
-  <button className={style.button}>
-    {auth ? auth : <LoginIcon className={style.svg} />}
-  </button>
-);
+export const Auth = ({token, delToken}) => {
+  const [auth, setAuth] = useState({});
+  const [btnClose, setBtnClose] = useState('dnone');
+  const toggleBtn = () => {
+    setBtnClose(btnClose === 'dnone' ? 'logout' : 'dnone');
+  };
+
+  useEffect(() => {
+    if (!token) return;
+    fetch(`${URL_API}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.status === 401) {
+          delToken;
+        } else {
+          return response.json();
+        }
+      })
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?.*$/, '');
+        setAuth({name, img});
+      })
+      .catch((err) => {
+        console.err(err);
+        setAuth({});
+      });
+  });
+  return (
+    <div className={style.container}>
+      {auth.name ? (
+        <>
+          <button onClick={toggleBtn} className={style.btn}>
+            <img
+              className={style.img}
+              src={auth.img}
+              title={auth.name}
+              alt={`Аватар ${auth.name}`}
+            />
+            <Text color="red">{auth.name}</Text>
+          </button>
+
+          <button onClick={delToken} className={style[btnClose]}>
+            <Text As="a" href={'http://localhost:3000'}>
+              Выйти
+            </Text>
+          </button>
+        </>
+      ) : (
+        <Text As="a" href={urlAuth} className={style.authLink}>
+          <LoginIcon className={style.svg} />
+        </Text>
+      )}
+    </div>
+  );
+};
 
 Auth.propTypes = {
-  auth: PropTypes.bool,
+  token: PropTypes.string,
+  delToken: PropTypes.any,
 };
